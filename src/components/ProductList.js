d5fad211a72200ba5ad6d5417fcbdeb7f5f3b000
BHObserver <BHObserver0@gmail.com>
2024-06-08 04:33:30 +0600

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,44 +1,69 @@
-// ProductList.js
-
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Table, TableHead, TableBody, TableRow, TableCell,
+  Paper, TableContainer, Typography, IconButton, Tooltip, Pagination,
+} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const ProductList = ({ products, onEdit, onDelete }) => (
-  <div>
-    {products.map((product) => (
-      <div key={`product-${product.id}`}>
-        <h3>{product.name}</h3>
-        <p>
-          Brand:
-          {product.brand}
-        </p>
-        <p>
-          Type:
-          {product.type}
-        </p>
-        <p>
-          Origin:
-          {product.origin}
-        </p>
-        <ul>
-          {product.variants.map((variant) => (
-            <li key={`variant-${variant.id}`}>
-              Color:
-              {' '}
-              {variant.color}
-              , Specification:
-              {' '}
-              {variant.specification}
-              , Size:
-              {' '}
-              {variant.size}
-            </li>
+const ProductList = ({
+  products, onEdit, onDelete, totalPages, currentPage, onPageChange,
+}) => (
+  <div style={{ padding: '20px' }}>
+    <Typography variant="h4" gutterBottom>
+      Product List
+    </Typography>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Brand</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Created At</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.id}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.brand}</TableCell>
+              <TableCell>{product.type}</TableCell>
+              <TableCell>{product.createdAt}</TableCell>
+              <TableCell>
+                <Tooltip title="Edit">
+                  <IconButton
+                    color="primary"
+                    onClick={() => onEdit(product)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Delete">
+                  <IconButton
+                    color="secondary"
+                    onClick={() => onDelete(product.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
+              </TableCell>
+            </TableRow>
           ))}
-        </ul>
-        <button type="button" onClick={() => onEdit(product)}>Edit</button>
-        <button type="button" onClick={() => onDelete(product.id)}>Delete</button>
-      </div>
-    ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    <Pagination
+      count={totalPages}
+      page={currentPage}
+      onChange={(e, page) => onPageChange(page)}
+      style={{ marginTop: 20, display: 'flex', justifyContent: 'center' }}
+    />
   </div>
 );
 
@@ -48,7 +73,7 @@ ProductList.propTypes = {
     name: PropTypes.string.isRequired,
     brand: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    origin: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
     variants: PropTypes.arrayOf(PropTypes.shape({
       id: PropTypes.number.isRequired,
       color: PropTypes.string.isRequired,
@@ -58,6 +83,9 @@ ProductList.propTypes = {
   })).isRequired,
   onEdit: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default ProductList;
