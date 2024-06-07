88a630e72ea0d5813274e40ccd06ae5566bbb3cd
BHObserver <BHObserver0@gmail.com>
2024-06-07 15:56:03 +0600

    Update ProductForm.js

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,5 +1,6 @@
-// src/components/ProductForm.js
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 function ProductForm({ onSubmit, product }) {
   const [name, setName] = useState(product ? product.name : '');
@@ -13,21 +14,35 @@ function ProductForm({ onSubmit, product }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </label>
-      <label>
-        Brand:
-        <input type="text" value={brand} onChange={(e) => setBrand(e.target.value)} />
-      </label>
-      <label>
-        Type:
-        <input type="text" value={type} onChange={(e) => setType(e.target.value)} />
-      </label>
+      <label htmlFor="name">Name:</label>
+      <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+      <label htmlFor="brand">Brand:</label>
+      <input id="brand" type="text" value={brand} onChange={(e) => setBrand(e.target.value)} />
+
+      <label htmlFor="type">Type:</label>
+      <input id="type" type="text" value={type} onChange={(e) => setType(e.target.value)} />
+
       <button type="submit">Submit</button>
     </form>
   );
 }
+
+ProductForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  product: PropTypes.shape({
+    name: PropTypes.string,
+    brand: PropTypes.string,
+    type: PropTypes.string,
+  }),
+};
+
+ProductForm.defaultProps = {
+  product: {
+    name: '',
+    brand: '',
+    type: '',
+  },
+};
 
 export default ProductForm;
