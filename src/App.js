d5b7633b3c3d2b5f678dce0dd6676b5196ae462e
BHObserver <BHObserver0@gmail.com>
2024-06-08 03:26:52 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import OrdersPage from './pages/OrdersPage';
 import ProductsPage from './pages/ProductsPage';
-import Navbar from './components/Navbar'; // Import Navbar
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<OrdersPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<OrdersPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
