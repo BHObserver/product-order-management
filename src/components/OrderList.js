1f1016e60fa668ba9d630cf2668a6d152c0bb687
BHObserver <BHObserver0@gmail.com>
2024-06-07 15:56:00 +0600

    Update OrderList.js

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,7 +1,11 @@
-// src/components/OrderList.js
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function OrderList({ orders, onEdit, onDelete }) {
+  if (!Array.isArray(orders)) {
+    return <p>No orders found.</p>;
+  }
+
   return (
     <div>
       <h2>Orders</h2>
@@ -15,13 +19,24 @@ function OrderList({ orders, onEdit, onDelete }) {
             {' '}
             -
             {order.total_quantity}
-            <button onClick={() => onEdit(order.id)}>Edit</button>
-            <button onClick={() => onDelete(order.id)}>Delete</button>
+            <button type="button" onClick={() => onEdit(order.id)}>Edit</button>
+            <button type="button" onClick={() => onDelete(order.id)}>Delete</button>
           </li>
         ))}
       </ul>
     </div>
   );
 }
+
+OrderList.propTypes = {
+  orders: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    email: PropTypes.string,
+    total_quantity: PropTypes.number,
+  })).isRequired,
+  onEdit: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default OrderList;
