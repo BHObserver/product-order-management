4fb2e51c742147799f66f5c673bff5bb142eb618
BHObserver <BHObserver0@gmail.com>
2024-06-08 23:54:53 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AppBar, Toolbar, Typography, Button,
+} from '@mui/material';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Orders</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-      </ul>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Inventory Management
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Products</Button>
+        <Button color="inherit" component={Link} to="/orders">Orders</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
