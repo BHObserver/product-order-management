7762fa2d59637a90f6039139b02a4fc113e9077f
BHObserver <BHObserver0@gmail.com>
2024-06-08 23:55:14 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Customize your primary color here
+    },
+    secondary: {
+      main: '#dc004e', // Customize your secondary color here
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif', // Customize your font family here
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 );
