7438d1b0e879675545b74eb38574a3d170476bee
BHObserver <BHObserver0@gmail.com>
2024-06-08 23:55:01 +0600

    Update OrderList.js

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,42 +1,64 @@
-// OrderList.js
-
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Table, TableHead, TableBody, TableRow, TableCell, Paper, TableContainer, Typography, IconButton,
+} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function OrderList({ orders, onEdit, onDelete }) {
+const OrderList = ({ orders, onEdit, onDelete }) => {
   if (orders.length === 0) {
-    return <p>No orders found.</p>;
+    return <Typography>No orders found.</Typography>;
   }
 
   return (
-    <div>
-      <h2>Orders</h2>
-      <ul>
-        {orders.map((order) => (
-          <li key={order.id}>
-            {order.name}
-            {' '}
-            -
-            {order.email}
-            {' '}
-            -
-            {order.total_quantity}
-            <button type="button" onClick={() => onEdit(order.id)}>Edit</button>
-            <button type="button" onClick={() => onDelete(order.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <TableContainer component={Paper}>
+      <Typography variant="h4" gutterBottom>
+        Orders
+      </Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Total Quantity</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orders.map((order) => (
+            <TableRow key={order.id}>
+              <TableCell>{order.id}</TableCell>
+              <TableCell>{order.name}</TableCell>
+              <TableCell>{order.email}</TableCell>
+              <TableCell>{order.total_quantity}</TableCell>
+              <TableCell>
+                <IconButton onClick={() => onEdit(order.id)}>
+                  <EditIcon />
+                </IconButton>
+                <IconButton onClick={() => onDelete(order.id)}>
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-}
+};
 
 OrderList.propTypes = {
-  orders: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-    email: PropTypes.string,
-    total_quantity: PropTypes.number,
-  })).isRequired,
+  orders: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      total_quantity: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
   onEdit: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
 };
