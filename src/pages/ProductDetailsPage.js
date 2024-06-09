a3502980094ad34243f3382b396795be1224c372
BHObserver <BHObserver0@gmail.com>
2024-06-10 01:40:12 +0600

    Update ProductDetailsPage.js

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -4,6 +4,8 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
 import { fetchProduct } from '../slices/productsSlice';
 import ProductDetails from '../components/ProductDetails';
 
@@ -11,18 +13,35 @@ const ProductDetailsPage = () => {
   const { productId } = useParams();
   const dispatch = useDispatch();
   const product = useSelector((state) => state.products.items[productId]);
+  const loading = useSelector((state) => state.products.loading);
+  const error = useSelector((state) => state.products.error);
 
   useEffect(() => {
-    dispatch(fetchProduct(productId));
+    if (productId) {
+      dispatch(fetchProduct(productId));
+    }
   }, [dispatch, productId]);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  if (error) {
+    return (
+      <Typography color="error">
+        Error:
+        {error}
+      </Typography>
+    );
+  }
+
+  if (!product) {
+    return <Typography>No product found</Typography>;
+  }
 
   return (
     <div>
-      {product ? (
-        <ProductDetails product={product} />
-      ) : (
-        <p>Loading...</p>
-      )}
+      <ProductDetails product={product} />
     </div>
   );
 };
